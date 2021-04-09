d60170bfd3445db000aba6f437a81946e6af2f41
Pranil Rathod <rathod1337@gmail.com>
2021-04-10 01:54:20 +0530

    #Refactor - added axios support

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import './helpers'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
