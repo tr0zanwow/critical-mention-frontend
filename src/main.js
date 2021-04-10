b6df66b11b5b2559b9e73515afe95e7ee310e292
Pranil Rathod <rathod1337@gmail.com>
2021-04-10 20:41:56 +0530

    #Refactor - initialized moment.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import './helpers'
 import store from './store'
+import moment from 'moment'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
+Vue.prototype.moment = moment
 
 new Vue({
   store,
