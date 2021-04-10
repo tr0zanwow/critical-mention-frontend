0994611e1187366658c366471e9c6055d93acb76
Pranil Rathod <rathod1337@gmail.com>
2021-04-10 20:42:43 +0530

    #Refactor - added method to get temprature unit on single parameter

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,15 @@
-Number.prototype.unitCelsius = function(){
-    return this.valueOf().toFixed(0) + ' \u00B0C'
+Number.prototype.unitCelsius = function() {
+  return this.valueOf().toFixed(0) + ' \u00B0C'
 }
 
-Number.prototype.unitFahrenheit = function(){
-    return ((this.valueOf() * (9/5)) + 32).toFixed(0) + ' \u00B0F'
+Number.prototype.unitFahrenheit = function() {
+  return (this.valueOf() * (9 / 5) + 32).toFixed(0) + ' \u00B0F'
+}
+
+Number.prototype.getTempratureInUnit = function({ fahrenheit = false }) {
+  if (fahrenheit) {
+    return this.valueOf().unitFahrenheit()
+  } else {
+    return this.valueOf().unitCelsius()
+  }
 }
