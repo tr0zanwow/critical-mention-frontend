7de5eeb99325076008e657451215b16e1668d8d8
Pranil Rathod <rathod1337@gmail.com>
2021-04-20 00:21:16 +0530

    #Refactor - changed base url for production

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,6 @@ module.exports = {
     },
   },
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/criticalmention/' : '/',
+    process.env.NODE_ENV === 'production' ? '/weatherapp/' : '/',
   runtimeCompiler: true,
 }
